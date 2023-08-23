5b08dcfc0d9934fcea05d2238cf8890b9d11d7f5
Josa Gesell <josa@gesell.me>
2023-08-23 15:00:11 +0200

    fix(shfmt): use proper indention

--- lua/code-commands/builtin/formatters/shfmt.lua
+++ lua/code-commands/builtin/formatters/shfmt.lua
@@ -1,15 +1,17 @@
 local M = {
   name = 'shfmt',
   cmd = 'shfmt',
-  args = {
-    '--indent=2', -- 0 for tabs (default), >0 for number of spaces
-    '--binary-next-line', -- binary ops like && and | may start a line
-    '--case-indent', -- switch cases will be indented
-    '--space-redirects', -- redirect operators will be followed by a space
-    -- '--keep-padding', -- keep column alignment paddings
-    -- '--func-next-line', -- function opening braces are placed on a separate line
-    '--filename=$FILENAME',
-  },
+  args = function(opts)
+    return {
+      ('--indent=%d'):format(vim.bo[opts.buffer].shiftwidth), -- 0 for tabs (default), >0 for number of spaces
+      '--binary-next-line', -- binary ops like && and | may start a line
+      '--case-indent', -- switch cases will be indented
+      '--space-redirects', -- redirect operators will be followed by a space
+      -- '--keep-padding', -- keep column alignment paddings
+      -- '--func-next-line', -- function opening braces are placed on a separate line
+      '--filename=$FILENAME',
+    }
+  end,
   stdin = true,
 }
 
